0ac88847bed020586ecc2cbfd654a47cfbcd27de
hadashamerovv <hamerovv@gmail.com>
2026-03-23 00:11:09 +0200

    site structure with folders

---
+++ js/header.js
@@ -0,0 +1,25 @@
+document.write(`
+<header class="site-header">
+    <div class="header-container">
+
+        <div class="logo">
+            <a href="index.html">
+                <img src="logo.png" alt="ספרות זולה">
+            </a>
+        </div>
+
+        <nav class="main-menu">
+            <a href="index.html">דף הבית</a>
+            <a href="about.html">אודות דוד אוראל</a>
+            <a href="perek.html">לקריאת פרק מהספר</a>
+            <a href="news.html">עדכונים וחדשות</a>
+            <a href="contact.html">צרו קשר</a>
+        </nav>
+
+        <div class="cart-area">
+            🛒 סל (0) 🔒 Visa • Mastercard
+        </div>
+
+    </div>
+</header>
+`);
