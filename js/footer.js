685be4db012cecfc79d0d9750b339ca935b93525
hadashamerovv <hamerovv@gmail.com>
2026-03-23 00:35:48 +0200

    footer and accessibility button again

--- js/footer.js
+++ js/footer.js
@@ -2,18 +2,12 @@ document.write(`
 <footer class="site-footer">
     <div class="footer-content">
 
-        <div class="footer-menu">
-            <a href="index.html">דף הבית</a>
-            <a href="about.html">אודות</a>
-            <a href="perek.html">פרק לקריאה</a>
-            <a href="news.html">חדשות</a>
-            <a href="contact.html">צור קשר</a>
-        </div>
-
         <div class="footer-text">
             © ספרות זולה | כל הזכויות שמורות
         </div>
 
     </div>
 </footer>
+
+<script src="https://cdn.enable.co.il/licenses/enable-L54576n9sqzi9jeh-0326-80874/init.js"></script>
 `);
